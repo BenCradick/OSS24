a2a0e66a29661edbb192a00affe3eca6020e3f65
Ben Cradick <ben.cradick@gmail.com>
2024-02-02 00:19:53 +0000

    Implemented arguments, added getopt skeleton.
    oss.c: Added getopt skeleton.
    user.c: implemented arguments to main for later.

--- oss.c
+++ oss.c
@@ -2,9 +2,25 @@
 #include <unistd.h>
 
 int main(int argc,  char* argv[]){
-    // int opt;
+    int opt;
 
-    // while(opt = getopt(argc, argv, )
+    while(opt = getopt(argc, argv, "nst") != -1){
+        // opt arguments -n, -s, -t
+        switch(opt){
+            case 'n':
+                printf("Option n\n");
+                break;
+            case 's':
+                printf("Option s\n");
+                break;
+            case 't':
+                printf("Option t\n");
+                break;
+            default:
+                printf("Option ?\n");
+                break;
+        }
+    }
 
      printf("Hello, World!\n");
     return 0;
