769acf229c7dda525a1330cdb03ef623b0e892a0
Ben Cradick <Ben.Cradick@gmail.com>
2024-02-03 20:34:58 +0000

    Implemented getopt and the user file.
    oss.c: implemented getopt to store off variabels in appropriate integers.
    user.c: Implemented all requirements per the project assignment list pid ppid iteration -> sleep -> list pid ppidp iteration.
    
    launch.json: vscode workspace implementation details, starting to work on getting gdb to work.
    task.json: vscode workpace implementation details, tasks tell the debug extension how to work, but still fifguring this out as a true debugger isn't really necessary on this project yet.

--- oss.c
+++ oss.c
@@ -1,24 +1,44 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <getopt.h>
+#include <stdlib.h>
 
 int main(int argc,  char* argv[]){
-    int opt;
+    int Opt;
+    int TotalChildren = -1;
+    int MaxProcess = -1;
+    int Iterations = -1;
 
-    while(opt = getopt(argc, argv, "nst") != -1){
+
+    while((Opt = getopt(argc, argv, "n:s:t:h")) != -1){
         // opt arguments -n, -s, -t
-        switch(opt){
+        switch(Opt){
+            case 'h':
+                printf("./oss [-n proc] [-s simul] [-t iter] [-h]\n");
+                printf("proc: number of processes to run\n");
+                printf("simul: number of processes to run at a time\n");
+                printf("iter: number of iterations for user to run\n");
+                return 0;
             case 'n':
-                printf("Option n\n");
+                TotalChildren = atoi(optarg);
+                printf("Total Children: %d\n", TotalChildren);
                 break;
             case 's':
-                printf("Option s\n");
+                MaxProcess = atoi(optarg);
+                printf("Max Process: %d\n", MaxProcess);
                 break;
             case 't':
-                printf("Option t\n");
+                Iterations = atoi(optarg);
+                printf("Iterations: %d\n", Iterations);
                 break;
             default:
                 printf("Option ?\n");
                 break;
+        }
+
+        if(TotalChildren == -1 || MaxProcess == -1 || Iterations == -1){
+            printf("Please enter all the arguments\n");
+            return 1;
         }
     }
 
--- user.c
+++ user.c
@@ -1,3 +1,16 @@
-int main(int argc, char* argv[]){
+#include <stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdlib.h>
 
+int main(int argc, char* argv[]){
+    for(int i = 0; i < atoi(argv[1]); i++){
+        printf("USER PID: %d PPID: %d Iteration: %d before sleeping\n", getpid(), getppid(), i);
+
+        sleep(1);
+
+        printf("USER PID: %d PPID: %d Iteration: %d after sleeping\n", getpid(), getppid(), i);
+    }
+    return 0;
 }
